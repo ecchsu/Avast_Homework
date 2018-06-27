5b2e6ffda281827f09f17d4714cca6d3be508ebb
Eva Hsu <ecc.hsu@gmail.com>
2018-06-27 16:14:59 +0800

    Add screenshot and html report

--- Homework_2/Test_Avast.py
+++ Homework_2/Test_Avast.py
@@ -7,41 +7,27 @@ import os
 import HTMLReport
 import time
 import base64
+from HTMLReport import AddImage
 
 class Homework(unittest.TestCase):
 
 	page_load_time = 10
 	run_func_time = 1200
 
-	HOME_KEY = 3
+	screenshotDir = '%s//report//' % os.getcwd()
+	timestr = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
 
-	screenshotDir = '%s/' % os.getcwd()
-
-	@classmethod
-	def setUpClass(cls):
+	def setUp(self):
 		desired_caps = {}
 		desired_caps['platformName'] = 'Android'
 		desired_caps['platformVersion'] = '7.1.1'
 		desired_caps['deviceName'] = 'Nexus 5 API 25'
 		desired_caps['appPackage'] = 'com.avast.android.mobilesecurity'
 		desired_caps['appActivity'] = 'app.main.MainActivity'
-		cls.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-
-		cls.driver.find_element_by_id('com.avast.android.mobilesecurity:id/eula_accept').click()
-		WebDriverWait(cls.driver, cls.page_load_time).until(EC.element_to_be_clickable((By.ID, 'com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue')))
-		cls.driver.find_element_by_id('com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue').click()
-
-	def setUp(self):
-		self.driver.press_keycode(self.HOME_KEY)
-		self.driver.find_element_by_id('com.google.android.apps.nexuslauncher:id/all_apps_handle').click()
-		self.driver.find_element_by_accessibility_id("Avast Mobile Security").click()
+		self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
 	def tearDown(self):
-		self.driver.press_keycode(self.HOME_KEY)
-
-	@classmethod
-	def tearDownClass(cls):
-		cls.driver.quit()
+		self.driver.quit()
 
 	def test_scan(self):
 		''' Step:
@@ -49,8 +35,8 @@ class Homework(unittest.TestCase):
 			2. Start scan
 			3. Wait for scan to complete
 		'''
-		# self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/eula_accept').click()
-		# self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue').click()
+		self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/eula_accept').click()
+		self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue').click()
 		self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/main_progress_button_button').click()
 		WebDriverWait(self.driver, self.page_load_time).until(EC.element_to_be_clickable((By.ID, 'com.android.packageinstaller:id/permission_allow_button')))
 		self.driver.find_element_by_id('com.android.packageinstaller:id/permission_allow_button').click()
@@ -58,10 +44,11 @@ class Homework(unittest.TestCase):
 		while True:
 			try: 
 				self.driver.find_element_by_xpath('//android.widget.Button[@text="RESCAN"]')
+				self.driver.save_screenshot(self.screenshotDir + 'test_scan %s.png' % self.timestr)
 			except:
 				wait_time += 1
 				if wait_time == self.run_func_time:
-					self.driver.save_screenshot(self.screenshotDir + 'test_scan.png')
+					self.driver.save_screenshot(self.screenshotDir + 'test_scan %s.png' % self.timestr)
 					raise Exception('Scan failed')
 			else:
 				break
@@ -74,9 +61,9 @@ class Homework(unittest.TestCase):
 			2. Go to Settings>About
 			3. Check app version
 		'''
-		# self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/eula_accept').click()
-		# WebDriverWait(self.driver, self.page_load_time).until(EC.element_to_be_clickable((By.ID, 'com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue')))
-		# self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue').click()
+		self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/eula_accept').click()
+		WebDriverWait(self.driver, self.page_load_time).until(EC.element_to_be_clickable((By.ID, 'com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue')))
+		self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue').click()
 		self.driver.find_element_by_accessibility_id('Open menu').click()
 		WebDriverWait(self.driver, self.page_load_time).until(EC.element_to_be_clickable((By.ID, 'com.avast.android.mobilesecurity:id/drawer_promo_asl')))
 		x = self.driver.get_window_size()['width']  
@@ -96,9 +83,9 @@ class Homework(unittest.TestCase):
 			2. Start boost RAM function
 			3. Wait for boost function to complete
 		'''
-		# self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/eula_accept').click()
-		# WebDriverWait(self.driver, self.page_load_time).until(EC.element_to_be_clickable((By.ID, 'com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue')))
-		# self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue').click()
+		self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/eula_accept').click()
+		WebDriverWait(self.driver, self.page_load_time).until(EC.element_to_be_clickable((By.ID, 'com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue')))
+		self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue').click()
 		self.driver.find_element_by_xpath('//android.widget.TextView[@text="BOOST RAM"]').click()
 		wait_time = 0
 		while True:
@@ -119,9 +106,9 @@ class Homework(unittest.TestCase):
 			1. Launch Avast Antivirus 2018 
 			2. Start clean junk function
 		'''
-		# self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/eula_accept').click()
-		# WebDriverWait(self.driver, self.page_load_time).until(EC.element_to_be_clickable((By.ID, 'com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue')))
-		# self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue').click()
+		self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/eula_accept').click()
+		WebDriverWait(self.driver, self.page_load_time).until(EC.element_to_be_clickable((By.ID, 'com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue')))
+		self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue').click()
 		self.driver.find_element_by_xpath('//android.widget.TextView[@text="CLEAN JUNK"]').click()
 		self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/cleanup_permission_button').click()
 		WebDriverWait(self.driver, self.page_load_time).until(EC.element_to_be_clickable((By.ID, 'com.android.packageinstaller:id/permission_allow_button')))
@@ -143,9 +130,9 @@ class Homework(unittest.TestCase):
 			1. Launch Avast Antivirus 2018 
 			2. Start scan Wi-Fi function
 		'''
-		# self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/eula_accept').click()
-		# WebDriverWait(self.driver, self.page_load_time).until(EC.element_to_be_clickable((By.ID, 'com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue')))
-		# self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue').click()
+		self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/eula_accept').click()
+		WebDriverWait(self.driver, self.page_load_time).until(EC.element_to_be_clickable((By.ID, 'com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue')))
+		self.driver.find_element_by_id('com.avast.android.mobilesecurity:id/btn_interstitial_remove_ads_continue').click()
 		self.driver.find_element_by_xpath('//android.widget.TextView[@text="SCAN WI-FI"]').click()
 		wait_time = 0
 		while True:
@@ -161,13 +148,12 @@ class Homework(unittest.TestCase):
 
 if __name__ == '__main__':
 	suite = unittest.TestSuite()
-	# suite.addTest(Homework('test_scan'))
-	# suite.addTest(Homework('test_appVersionNotEmpty'))
+	suite.addTest(Homework('test_scan'))
+	suite.addTest(Homework('test_appVersionNotEmpty'))
 	suite.addTest(Homework('test_boostRam'))
-	# suite.addTest(Homework('test_cleanJunk'))
-	# suite.addTest(Homework('test_scanWifi'))
-	timestr = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
-	runner = HTMLReport.TestRunner(report_file_name = 'Avast Antivirus 2018' + timestr,
+	suite.addTest(Homework('test_cleanJunk'))
+	suite.addTest(Homework('test_scanWifi'))
+	runner = HTMLReport.TestRunner(report_file_name = 'Avast Antivirus 2018 ' + Homework.timestr,
 									output_path ='Report',
 									description = 'App automation test for Avast Antivirus 2018 basic functions',
 									thread_count = 1,
